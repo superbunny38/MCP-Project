257211495e20839bfa513ea24c08a7cb567dc09a
superbunny38 <superbunny38@gmail.com>
2025-05-17 17:44:09 -0400

    update tools and resources

--- solver_server.py
+++ solver_server.py
@@ -9,7 +9,25 @@ WiKI_DIR = "/Users/chaeeunryu/Desktop/MCP Study/ToyProject/MCP-Project/Wikis"
 # Initialize the MCP server
 mcp = FastMCP("Demo Server")
 
-@mcp.prompt
+@mcp.resource("CodeBase://folders")
+def get_available_codes() -> str:
+    """
+    List all available codes in the CodeBase.
+    
+    This resource provides a simple list of all codes.
+    """
+    
+    codes = []
+    
+    # Get all topic directories.
+
+@mcp.tool()
+def extract_info_about_code(code_name: str) -> str:
+    """
+    
+    """
+
+@mcp.prompt()
 def generate_wiki_related_prompt(topic: str, ticket_id: int=5) -> str:
     """
     Generate a prompt for the client to find the solution referring to the wikis
