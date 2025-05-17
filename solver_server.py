5636eeab80f74b100b4a57f5f9696c5b556fa6e9
Chaeeun Ryu <chaeeunryu@chaeeuns-air.lan>
2025-05-17 17:35:00 -0400

    prompt updated

--- solver_server.py
+++ solver_server.py
@@ -9,3 +9,15 @@ WiKI_DIR = "/Users/chaeeunryu/Desktop/MCP Study/ToyProject/MCP-Project/Wikis"
 # Initialize the MCP server
 mcp = FastMCP("Demo Server")
 
+
+@mcp.prompt
+def generate_wiki_related_prompt(topic: str, ticket_id: int=5) -> str:
+    """
+    Generate a prompt for the client to find the solution referring to the wikis
+    on a specific problem.
+    """
+    
+    prompt_str = """
+    
+    """
+    return prompt_str
