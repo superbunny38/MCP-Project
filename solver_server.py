bc04a780e3613d5138d38264f82f062c6d58ba76
Chaeeun Ryu <chaeeunryu@chaeeuns-air.lan>
2025-05-17 17:24:22 -0400

    server

--- solver_server.py
+++ solver_server.py
@@ -3,3 +3,9 @@ import json
 import arxiv   
 from typing import List
 from mcp.server.fastmcp import FastMCP
+
+WiKI_DIR = "/Users/chaeeunryu/Desktop/MCP Study/ToyProject/MCP-Project/Wikis"
+
+# Initialize the MCP server
+mcp = FastMCP("Demo Server")
+
